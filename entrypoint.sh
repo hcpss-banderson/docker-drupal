af39c6b4fbaca1d3870f89a8a78a65996a375525
Brendan Anderson <brendan_anderson@hcpss.org>
2022-10-12 10:35:38 -0400

    Add onbuild

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -d /var/www/drupal/files ]; then
+    mkdir -p /var/www/drupal/files
+fi
+
+if [ ! -d /var/www/drupal/web/sites/default/files ]; then
+    mkdir -p /var/www/drupal/web/sites/default/files
+fi
+
+chown -R www-data:www-data /var/www/drupal/files
+chown -R www-data:www-data /var/www/drupal/config
+chown -R www-data:www-data /var/www/drupal/web/sites/default/files
+
+chown root:root /var/www/drupal/web/sites/default/settings.php
+chmod 444 /var/www/drupal/web/sites/default/settings.php
+
+# Wait for MySQL
+while ! mysqladmin ping -h"$MYSQL_HOSTNAME" --silent; do
+    echo "Waiting for database connection..."
+    sleep 5
+done
+
+drush --root=/var/www/drupal/web cc drush
+drush --root=/var/www/drupal/web cr
+drush --root=/var/www/drupal/web cim -y
+drush --root=/var/www/drupal/web cr
+drush --root=/var/www/drupal/web updb -y
+
+exec "$@"
